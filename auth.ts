269ec3aed0e747eb95b618ceebcbcca8e61a06ed
Erick Augusto <erickkuiz@gmail.com>
2024-07-30 12:36:01 -0300

    first commit

---
+++ auth.ts
@@ -0,0 +1,72 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { compareSync } from 'bcrypt-ts';
+import db from './lib/db';
+import GithubProvider from 'next-auth/providers/github';
+import EmailProvider from 'next-auth/providers/nodemailer';
+import { PrismaAdapter } from '@auth/prisma-adapter';
+
+
+declare module 'next-auth' {
+  interface Session {
+    user: User & {
+      githubProfile: any;
+    };
+  }
+}
+
+export const {
+  handlers: { GET, POST },
+  signIn,
+  signOut,
+  auth
+} = NextAuth({
+  providers: [
+    GithubProvider({ allowDangerousEmailAccountLinking: true }),
+    Credentials({
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'email@exemplo.com.br',
+        },
+        password: { label: 'Senha', type: 'password' },
+      },
+      async authorize(credentials) {
+        const email = credentials.email as string;
+        const password = credentials.password as string;
+
+        if (!email || !password) {
+          return null;
+        }
+
+        const user = await db.user.findUnique({
+          where: {
+            email: email,
+          },
+        });
+
+        if (!user) {
+          return null;
+        }
+
+        const passwordMatch = compareSync(password, user.password ?? '');
+        if (!passwordMatch) {
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, profile }) {
+      return { githubProfile: profile, ...token };
+    },
+
+    async session({ session, token }) {
+      session.user.githubProfile = token.githubProfile;
+      return session;
+    },
+  },
+});
